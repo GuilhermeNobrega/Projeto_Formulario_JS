3c6633648efa53b12d272e071ad2409a12f61b9d
Guilherme Nóbrega Gomes Dantas <100214869+GuilhermeNobrega@users.noreply.github.com>
2023-06-27 11:54:04 -0300

    Add files via upload

---
+++ recebedor.js
@@ -0,0 +1,11 @@
+/*Neste algoritmo, iremos receber dados e 
+ver os signos de acordo com as datas
+*/
+console.log("Vamos codar")
+var nome = window.prompt("Digite seu nome:")
+if(nome==false){
+    console.log("Nome esta invalido")
+}
+else{
+    console.log("Olá",nome)
+}
